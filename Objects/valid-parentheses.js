d5c498b055fe63d0e61388620f3ce34adb1daf67
Sourabh <62754147+Sourabh-Kulkarni@users.noreply.github.com>
2022-09-13 09:17:23 +0530

    Create valid-parentheses.js

---
+++ Objects/valid-parentheses.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+
+var isValid = function(s) {
+  const left = [];
+  const legend = {
+    '(': ')',
+    '{': '}',
+    '[': ']'
+  };
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
+      left.push(s[i]);    
+    } else if (legend[left.pop()] !== s[i]) {
+      return false;
+    }
+  }
+  return left.length ? 0 : 1;
+
+}
